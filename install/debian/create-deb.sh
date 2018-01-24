99e9b876785b4945ea04ad955bbf8de58ca4c0bf
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-01-24 23:30:47 +0200

    Support many architectures.

--- install/debian/create-deb.sh
+++ install/debian/create-deb.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
 # Script for creating Debian package.
+#
+# ./create-deb.sh [arch]
+# Where arch is one of "amd64", "x86_64", "i386".
+# Argument arch is optional, current host architecture will be
+# used for package creation.
 
-( cd ../../ && make linux64-release ) || exit
+if [ $# = 0 ]; then
+   ARCH=$(arch)
+else
+   ARCH=$1
+fi
+case $ARCH in
+     x86_64|amd64)
+          target=64
+          ARCH=amd64
+          ;;
+     i386)
+          target=32
+          ;;
+     *)
+          echo "Unsupported architecture $ARCH"
+          exit
+esac
+
+( cd ../../ && make linux${target}-release ) || exit
 
 TMP=$(mktemp -d)
 VER=$(grep APP_VER ../../source/version.inc|grep -o "'.*'"|sed "s/'//g")
@@ -13,7 +36,7 @@ mkdir -p $TMP/usr/share/applications
 mkdir -p $TMP/usr/share/pixmaps
 mkdir -p $TMP/DEBIAN
 
-cp control $TMP/DEBIAN
+sed "s/@arch@/$ARCH/" control > $TMP/DEBIAN/control
 cp ../resources/http-inspector.desktop $TMP/usr/share/applications
 cp ../resources/http-inspector.png $TMP/usr/share/pixmaps
 cp ../../bin/x86_64-linux/http-inspector $TMP/usr/bin
