7da342dc26160dc11aa3b7ee4270b1c974ae18ce
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-06-16 19:42:11 +0300

    Code format.

--- source/thread_http_client.pas
+++ source/thread_http_client.pas
@@ -31,12 +31,12 @@ type
     RequestHeaders: TStrings;
     ResponseHeaders: TStrings;
     Content: TStringStream;
-    ContentType: String;
-    Time: Int64; // time of request execution in milliseconds
+    ContentType: string;
+    Time: int64; // time of request execution in milliseconds
   end;
 
-  TOnRequestComplete = procedure (ResponseInfo: TResponseInfo) of object;
-  TOnException = procedure (Url, Method: string; E: Exception) of object;
+  TOnRequestComplete = procedure(ResponseInfo: TResponseInfo) of object;
+  TOnException = procedure(Url, Method: string; E: Exception) of object;
 
   { TThreadHttpClient }
 
@@ -60,23 +60,24 @@ type
     procedure OnClientException;
   protected
     procedure Execute; override;
-    function ParseContentType: String;
+    function ParseContentType: string;
   public
-    constructor Create(CreateSuspened: Boolean);
+    constructor Create(CreateSuspened: boolean);
     destructor Destroy; override;
-    procedure AddHeader(const AHeader,AValue : String);
-    procedure AddCookie(const AName, AValue : String; EncodeValue: Boolean = True);
+    procedure AddHeader(const AHeader, AValue: string);
+    procedure AddCookie(const AName, AValue: string; EncodeValue: boolean = True);
     property Client: TCustomHttpClient read FHttpClient;
     property Method: string read FHttpMethod write SetHttpMethod;
     property Url: string read FUrl write SetUrl;
     property RequestBody: TStream read GetRequestBody write SetRequestBody;
-    property OnRequestComplete: TOnRequestComplete read FOnRequestComplete write FOnRequestComplete;
+    property OnRequestComplete: TOnRequestComplete
+      read FOnRequestComplete write FOnRequestComplete;
     property OnException: TOnException read FOnClientException write FOnClientException;
   end;
 
-  function DecodeUrl(const url: string): string;
-  function GetURLQueryParams(const url: string): TQueryParams;
-  function ReplaceURLQueryParams(const url: string; Params: TQueryParams): string;
+function DecodeUrl(const url: string): string;
+function GetURLQueryParams(const url: string): TQueryParams;
+function ReplaceURLQueryParams(const url: string; Params: TQueryParams): string;
 
 implementation
 
@@ -95,7 +96,7 @@ function GetURLQueryParams(const url: string): TQueryParams;
 var
   URI: TURI;
   Params, KV: TStringList;
-  I: Integer;
+  I: integer;
 begin
   Result := TQueryParams.Create;
   Params := TStringList.Create;
@@ -103,12 +104,14 @@ begin
   try
     URI := ParseURI(url);
     SplitStrings(URI.Params, '&', Params);
-    for I := 0 to Params.Count - 1 do begin
+    for I := 0 to Params.Count - 1 do
+    begin
       SplitStrings(Params[I], '=', KV);
       case KV.Count of
         1: Result.Add(DecodeURLElement(KV[0]), '');
         // Fix: DecodeURLElement on empty string leads to use the value from the previous iteration.
-        2: Result.Add(DecodeURLElement(KV[0]), IfThen(KV[1] = '', '', DecodeURLElement(KV[1])));
+        2: Result.Add(DecodeURLElement(KV[0]), IfThen(KV[1] = '', '',
+            DecodeURLElement(KV[1])));
       end;
     end;
   finally
@@ -123,18 +126,19 @@ var
   ParamStr, Key, Data: string;
   I: integer;
 begin
-  ParamStr:='';
-  for I:=0 to Params.Count-1 do begin
-    Key:=Trim(Params.Keys[I]);
-    Data:=Params.Data[I];
+  ParamStr := '';
+  for I := 0 to Params.Count - 1 do
+  begin
+    Key := Trim(Params.Keys[I]);
+    Data := Params.Data[I];
     if Key <> '' then
-      ParamStr:=ParamStr + IfThen(Data = '', Key, Format('%s=%s', [Key, Data])) + '&';
+      ParamStr := ParamStr + IfThen(Data = '', Key, Format('%s=%s', [Key, Data])) + '&';
   end;
-  ParamStr:=TrimRightSet(ParamStr, ['&']);
+  ParamStr := TrimRightSet(ParamStr, ['&']);
 
-  URI:=ParseURI(url);
-  URI.Params:=ParamStr;
-  Result:=EncodeURI(URI);
+  URI := ParseURI(url);
+  URI.Params := ParamStr;
+  Result := EncodeURI(URI);
 end;
 
 { TCustomHttpClient }
@@ -143,23 +147,27 @@ procedure TCustomHttpClient.MultiFileStreamFormPost(FormData, FileNames: TString
 var
   N, V, S, Sep: string;
   SS: TStringStream;
-  I: Integer;
+  I: integer;
   FS: TFileStream;
 begin
   Sep := Format('-----%.8x-%.8x_multipart_boundary', [Random($ffffff), Random($ffffff)]);
-  AddHeader('Content-Type','multipart/form-data; boundary=' + Sep);
+  AddHeader('Content-Type', 'multipart/form-data; boundary=' + Sep);
   SS := TStringStream.Create('');
   if (FormData <> nil) then
-    for I:=0 to FormData.Count - 1 do begin
+    for I := 0 to FormData.Count - 1 do
+    begin
       FormData.GetNameValue(I, N, V);
       S := '--' + Sep + CRLF;
-      S := S + Format('Content-Disposition: form-data; name="%s"' + CRLF + CRLF + '%s' + CRLF, [N, V]);
+      S := S + Format('Content-Disposition: form-data; name="%s"' +
+        CRLF + CRLF + '%s' + CRLF, [N, V]);
       SS.WriteBuffer(S[1], Length(S));
     end;
-  for I := 0 to FileNames.Count - 1 do begin
+  for I := 0 to FileNames.Count - 1 do
+  begin
     S := '--' + Sep + CRLF;
     FileNames.GetNameValue(I, N, V);
-    S := S + Format('Content-Disposition: form-data; name="%s"; filename="%s"'+CRLF, [N, ExtractFileName(V)]);
+    S := S + Format('Content-Disposition: form-data; name="%s"; filename="%s"' + CRLF,
+      [N, ExtractFileName(V)]);
     S := S + 'Content-Type: application/octet-string' + CRLF + CRLF;
     SS.WriteBuffer(S[1], Length(S));
     try
@@ -167,7 +175,8 @@ begin
       FS.Seek(0, soFromBeginning);
       SS.CopyFrom(FS, FS.Size);
       FreeAndNil(FS);
-    except on E: Exception do
+    except
+      on E: Exception do
       begin
         //FreeAndNil(FS);
         FreeAndNil(SS);
@@ -191,9 +200,11 @@ end;
 
 procedure TThreadHttpClient.SetHttpMethod(AValue: string);
 begin
-  if FHttpMethod = AValue then Exit; // =>
+  if FHttpMethod = AValue then
+    Exit; // =>
   AValue := UpperCase(Trim(AValue));
-  if Length(AValue) = 0 then AValue := 'GET'; // GET by default.
+  if Length(AValue) = 0 then
+    AValue := 'GET'; // GET by default.
   FHttpMethod := AValue;
 end;
 
@@ -204,9 +215,11 @@ end;
 
 procedure TThreadHttpClient.SetUrl(AValue: string);
 begin
-  if FUrl = AValue then Exit; // =>
+  if FUrl = AValue then
+    Exit; // =>
   // TODO: need better protocol parser.
-  if Pos('http', AValue) = 0 then AValue := 'http://' + AValue;
+  if Pos('http', AValue) = 0 then
+    AValue := 'http://' + AValue;
   FUrl := AValue;
 end;
 
@@ -216,29 +229,31 @@ var
 begin
   if Assigned(FOnRequestComplete) then
   begin
-    info.Method:=FHttpMethod;
-    info.Url:=FUrl;
-    info.RequestHeaders:=FHttpClient.RequestHeaders;
-    info.ResponseHeaders:=FHttpClient.ResponseHeaders;
-    info.StatusCode:=FHttpClient.ResponseStatusCode;
-    info.StatusText:=FHttpClient.ResponseStatusText;
-    info.HttpVersion:=FHttpClient.ServerHTTPVersion;
-    info.Content:=FResponseData;
-    info.Time:=MilliSecondsBetween(FFinishTime, FStartTime);
-    info.ContentType:=ParseContentType;
+    info.Method := FHttpMethod;
+    info.Url := FUrl;
+    info.RequestHeaders := FHttpClient.RequestHeaders;
+    info.ResponseHeaders := FHttpClient.ResponseHeaders;
+    info.StatusCode := FHttpClient.ResponseStatusCode;
+    info.StatusText := FHttpClient.ResponseStatusText;
+    info.HttpVersion := FHttpClient.ServerHTTPVersion;
+    info.Content := FResponseData;
+    info.Time := MilliSecondsBetween(FFinishTime, FStartTime);
+    info.ContentType := ParseContentType;
     FOnRequestComplete(info);
   end;
 end;
 
 procedure TThreadHttpClient.OnClientException;
 begin
-  if Assigned(FOnClientException) then FOnClientException(Url, Method, FException);
+  if Assigned(FOnClientException) then
+    FOnClientException(Url, Method, FException);
 end;
 
 procedure TThreadHttpClient.Execute;
 begin
   try
-    if Assigned(FCookies) then FHttpClient.Cookies := FCookies;
+    if Assigned(FCookies) then
+      FHttpClient.Cookies := FCookies;
     FStartTime := Now;
     FHttpClient.HTTPMethod(FHttpMethod, FUrl, FResponseData, []);
     FFinishTime := Now;
@@ -252,18 +267,21 @@ begin
   end;
 end;
 
-function TThreadHttpClient.ParseContentType: String;
+function TThreadHttpClient.ParseContentType: string;
 var
-  I, P: Integer;
-  Line, Header: String;
+  I, P: integer;
+  Line, Header: string;
 begin
   Result := '';
-  for I := 0 to FHttpClient.ResponseHeaders.Count - 1 do begin
+  for I := 0 to FHttpClient.ResponseHeaders.Count - 1 do
+  begin
     Line := FHttpClient.ResponseHeaders.Strings[I];
     P := Pos(':', Line);
-    if p <> 0 then begin
+    if p <> 0 then
+    begin
       Header := LeftStr(Line, P - 1);
-      if LowerCase(Header) = 'content-type' then begin
+      if LowerCase(Header) = 'content-type' then
+      begin
         Result := Trim(RightStr(Line, Length(Line) - P));
         P := Pos(';', Result);
         if P <> 0 then
@@ -273,38 +291,39 @@ begin
   end;
 end;
 
-constructor TThreadHttpClient.Create(CreateSuspened: Boolean);
+constructor TThreadHttpClient.Create(CreateSuspened: boolean);
 begin
   inherited Create(CreateSuspened);
   FreeOnTerminate := True;
   FHttpClient := TCustomHttpClient.Create(nil);
   FResponseData := TStringStream.Create('');
-  FOnClientException:=nil;
-  FOnRequestComplete:=nil;
-  FCookies:=nil;
+  FOnClientException := nil;
+  FOnRequestComplete := nil;
+  FCookies := nil;
 end;
 
 destructor TThreadHttpClient.Destroy;
 begin
-  if Assigned(FCookies) then FCookies.Free;
+  if Assigned(FCookies) then
+    FCookies.Free;
   FHttpClient.RequestBody.Free;
   FreeAndNil(FHttpClient);
   FreeAndNil(FResponseData);
   inherited Destroy;
 end;
 
-procedure TThreadHttpClient.AddHeader(const AHeader, AValue: String);
+procedure TThreadHttpClient.AddHeader(const AHeader, AValue: string);
 begin
   FHttpClient.AddHeader(AHeader, AValue);
 end;
 
-procedure TThreadHttpClient.AddCookie(const AName, AValue: String; EncodeValue: Boolean = True);
+procedure TThreadHttpClient.AddCookie(const AName, AValue: string;
+  EncodeValue: boolean = True);
 begin
-  if not Assigned(FCookies) then FCookies := TStringList.Create;
-  FCookies.Add(Format('%s=%s', [
-    AName, IfThen(EncodeValue, EncodeURLElement(AValue), AValue)
-  ]));
+  if not Assigned(FCookies) then
+    FCookies := TStringList.Create;
+  FCookies.Add(Format('%s=%s',
+    [AName, IfThen(EncodeValue, EncodeURLElement(AValue), AValue)]));
 end;
 
 end.
-
