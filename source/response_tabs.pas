46efd1956c3331dc7beb8885b61809936323b6cf
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-06-20 22:37:33 +0300

    Move json tree builder to response tab.

--- source/response_tabs.pas
+++ source/response_tabs.pas
@@ -5,8 +5,8 @@ unit response_tabs;
 interface
 
 uses
-  Classes, SysUtils, contnrs, fpjson, ComCtrls, ExtCtrls, Controls, Forms,
-  StdCtrls, thread_http_client;
+  Classes, SysUtils, contnrs, fpjson, jsonparser, ComCtrls, ExtCtrls, Controls,
+  Forms, StdCtrls, thread_http_client;
 
 type
 
@@ -28,18 +28,24 @@ type
     property TabSheet: TTabSheet read FTabSheet;
   end;
 
+  { TOnOpenResponseTab }
+
+  TOnOpenResponseTab = procedure(Tab: TResponseTab; ResponseInfo: TResponseInfo) of object;
+
   { TResponseTabManager }
 
   TResponseTabManager = class
   private
     FPageControl: TPageControl;
     FTabs: TObjectList;
+    FOnOpenResponseTab: TOnOpenResponseTab;
   public
     constructor Create(APageControl: TPageControl);
     destructor Destroy; override;
     procedure RegisterTab(Tab: TResponseTab);
     procedure OpenTabs(ResponseInfo: TResponseInfo);
     property PageControl: TPageControl read FPageControl write FPageControl;
+    property OnOpenResponseTab: TOnOpenResponseTab read FOnOpenResponseTab write FOnOpenResponseTab;
   end;
 
   { TResponseImageTab }
@@ -66,8 +72,11 @@ type
   private
     FTreeView: TTreeView;
     FJsonRoot: TJSONData;
+    FJsonParser: TJSONParser;
     function GetTreeView: TTreeView;
     procedure LoadDocument(doc: string);
+    procedure ShowJsonData(AParent: TTreeNode; Data: TJSONData);
+    procedure ClearJsonData;
   public
     constructor Create;
     destructor Destroy; override;
@@ -75,17 +84,115 @@ type
     procedure CreateUI(ATabSheet: TTabSheet); override;
     procedure OnHttpResponse(ResponseInfo: TResponseInfo); override;
     procedure OnSaveResponse(const AFileName: string); override;
+    procedure FreeTab; override;
     property TreeView: TTreeView read GetTreeView;
   end;
 
 implementation
 
+const
+  ImageTypeMap: array[TJSONtype] of Integer =
+  // (jtUnknown, jtNumber, jtString, jtBoolean, jtNull, jtArray, jtObject)
+  (-1, 3, 2, 4, 5, 0, 1);
 
 { TResponseJsonTab }
 
 procedure TResponseJsonTab.LoadDocument(doc: string);
+var
+  S: TStringStream;
 begin
+  S := TStringStream.Create(doc);
+  FJsonParser := TJSONParser.Create(S);
+  with FTreeView.Items do begin
+    BeginUpdate;
+    try
+      FJsonRoot := FJsonParser.Parse;
+      ShowJsonData(nil, FJsonRoot);
+      with FTreeView do
+        if (Items.Count > 0) and Assigned(Items[0]) then begin
+          Items[0].Expand(False);
+          Selected := Items[0];
+        end;
+    finally
+      EndUpdate;
+    end;
+  end;
+  FreeAndNil(S);
+end;
 
+procedure TResponseJsonTab.ShowJsonData(AParent: TTreeNode; Data: TJSONData);
+var
+  N2: TTreeNode;
+  I: Integer;
+  D: TJSONData;
+  C: String;
+  S: TStringList;
+begin
+  if Not Assigned(Data) then
+    exit;
+
+  if not Assigned(AParent) then
+  begin
+    AParent := FTreeView.Items.AddChild(nil, '');
+    AParent.ImageIndex := ImageTypeMap[Data.JSONType];
+    AParent.SelectedIndex := ImageTypeMap[Data.JSONType];
+    AParent.Data := Data;
+  end;
+
+  Case Data.JSONType of
+    jtArray,
+    jtObject:
+      begin
+        If (Data.JSONType = jtArray) then
+          AParent.Text := AParent.Text + Format('[%d]', [Data.Count])
+        else if (Data.JSONType = jtObject) and (AParent.Text = '') then
+          AParent.Text := 'Object';
+        S := TstringList.Create;
+        try
+          for I:=0 to Data.Count-1 do
+            If Data.JSONtype = jtArray then
+              S.AddObject(IntToStr(I), Data.items[i])
+            else
+              S.AddObject(TJSONObject(Data).Names[i], Data.items[i]);
+          for I:=0 to S.Count-1 do
+            begin
+              N2 := FTreeView.Items.AddChild(AParent, S[i]);
+              D := TJSONData(S.Objects[i]);
+              N2.ImageIndex := ImageTypeMap[D.JSONType];
+              N2.SelectedIndex := ImageTypeMap[D.JSONType];
+              N2.Data := D;
+              ShowJSONData(N2, D);
+            end
+        finally
+          S.Free;
+        end;
+      end;
+    jtNull:
+      C := 'null';
+  else
+    if Data.JSONType = jtNumber then
+      C := Data.AsFloat.ToString
+    else
+      C := Data.AsString;
+    if (Data.JSONType = jtString) then
+      C := '"'+C+'"';
+    AParent.Text := AParent.Text + ': ' + C;
+    AParent.Data := Data;
+  end;
+end;
+
+procedure TResponseJsonTab.ClearJsonData;
+begin
+  if Assigned(FJsonRoot) then begin
+    case FJsonRoot.JSONType of
+      jtArray, jtObject: FJsonRoot.Clear;
+    end;
+    FreeAndNil(FJsonRoot);
+  end;
+  if Assigned(FTreeView) then
+    FTreeView.Items.Clear;
+  if Assigned(FJsonParser) then
+    FreeAndNil(FJsonParser);
 end;
 
 function TResponseJsonTab.GetTreeView: TTreeView;
@@ -98,12 +205,15 @@ end;
 constructor TResponseJsonTab.Create;
 begin
   inherited;
-  FTreeView := nil;
-  FJsonRoot := nil;
+  FName       := 'JSON';
+  FTreeView   := nil;
+  FJsonRoot   := nil;
+  FJsonParser := nil;
 end;
 
 destructor TResponseJsonTab.Destroy;
 begin
+  ClearJsonData;
   inherited Destroy;
 end;
 
@@ -122,11 +232,13 @@ begin
   FTreeView.ReadOnly := True;
   FTreeView.RightClickSelect := True;
   FTreeView.ScrollBars := ssAutoBoth;
+  FTreeView.ToolTips := False;
 end;
 
 procedure TResponseJsonTab.OnHttpResponse(ResponseInfo: TResponseInfo);
 begin
   if Assigned(FTreeView) then begin
+    ClearJsonData;
     LoadDocument(ResponseInfo.Content.DataString);
   end;
 end;
@@ -138,18 +250,25 @@ begin
   end;
 end;
 
+procedure TResponseJsonTab.FreeTab;
+begin
+  ClearJsonData;
+  inherited;
+end;
+
 { TResponseTabManager }
 
 constructor TResponseTabManager.Create(APageControl: TPageControl);
 begin
   FPageControl := APageControl;
   FTabs := TObjectList.Create;
+  FOnOpenResponseTab := nil;
 end;
 
 destructor TResponseTabManager.Destroy;
 begin
-  FreeAndNil(FPageControl);
   FreeAndNil(FTabs);
+  FreeAndNil(FPageControl);
   inherited Destroy;
 end;
 
@@ -171,6 +290,8 @@ begin
       if not Assigned(Tab.TabSheet) then
         Tab.CreateUI(FPageControl.AddTabSheet);
       Tab.OnHttpResponse(ResponseInfo);
+      if Assigned(FOnOpenResponseTab) then
+        FOnOpenResponseTab(Tab, ResponseInfo);
     end
     else
       Tab.FreeTab;
@@ -212,6 +333,7 @@ end;
 
 constructor TResponseImageTab.Create;
 begin
+  inherited;
   FName := 'Image';
   FImage := nil;
 end;
