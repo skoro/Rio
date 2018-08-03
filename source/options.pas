e85e8f801c4bba0295ab717c90aa81113fe79c5c
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-08-03 22:49:48 +0300

    Font selector ui.

--- source/options.pas
+++ source/options.pas
@@ -6,9 +6,12 @@ interface
 
 uses
   SysUtils, Forms, ExtCtrls, StdCtrls, JSONPropStorage, Spin, ComCtrls, fpjson,
-  PairSplitter, response_tabs;
+  PairSplitter, Dialogs, Graphics, response_tabs, Classes;
 
 type
+
+  TUIFontItem = (fiGrids, fiEditor, fiJson, fiContent, fiValue);
+  TFontItemList = array of TFont;
 
   { TOptionsPage }
 
@@ -18,16 +21,21 @@ type
 
   TOptionsForm = class(TForm)
     Button1: TButton;
+    btnSelectFont: TButton;
     cbJsonExpanded: TCheckBox;
     cbJsonSaveFmt: TCheckBox;
     cbJsonFmtArray: TCheckBox;
     cbHideGridButtons: TCheckBox;
+    cboxFontItem: TComboBox;
     editIndentSize: TSpinEdit;
+    dlgFont: TFontDialog;
     GroupBox1: TGroupBox;
     gbLayout: TGroupBox;
     GroupBox2: TGroupBox;
+    gbFonts: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
+    lFontDemo: TLabel;
     pagesOptions: TPageControl;
     Panel1: TPanel;
     Props: TJSONPropStorage;
@@ -38,8 +46,11 @@ type
     tabJson: TTabSheet;
     tabAppearance: TTabSheet;
     TabSheet2: TTabSheet;
+    procedure btnSelectFontClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
+    FFontItemList: TFontItemList;
+    function GetFontItem(aIndex: Integer): TFont;
     function GetGridButtonsHidden: Boolean;
     function GetJsonFormatOptions: TFormatOptions;
     function GetJsonIndentSize: Integer;
@@ -47,6 +58,9 @@ type
     function GetJsonSaveFmt: Boolean;
     function GetJsonView: TViewPage;
     function GetPanelsLayout: TPairSplitterType;
+    procedure SetFontDemo;
+    procedure SetFontItem(aIndex: Integer; AValue: TFont);
+    procedure InitFonts;
 
   public
     function ShowModalPage(page: TOptionsPage): TModalResult;
@@ -57,6 +71,7 @@ type
     property JsonFormat: TFormatOptions read GetJsonFormatOptions;
     property PanelsLayout: TPairSplitterType read GetPanelsLayout;
     property GridButtonsHidden: Boolean read GetGridButtonsHidden;
+    property FontItem[aIndex: Integer]: TFont read GetFontItem write SetFontItem;
   end;
 
 var
@@ -76,6 +91,19 @@ begin
   Props.JSONFileName := CF;
   Props.Active := True;
   pagesOptions.ActivePage := tabAppearance;
+  InitFonts;
+end;
+
+function TOptionsForm.GetFontItem(aIndex: Integer): TFont;
+begin
+
+end;
+
+procedure TOptionsForm.btnSelectFontClick(Sender: TObject);
+begin
+  if dlgFont.Execute then begin
+    //dlgFont.Font;
+  end;
 end;
 
 function TOptionsForm.GetGridButtonsHidden: Boolean;
@@ -119,6 +147,20 @@ begin
     Result := pstHorizontal
   else
     Result := pstVertical;
+end;
+
+procedure TOptionsForm.SetFontDemo;
+begin
+end;
+
+procedure TOptionsForm.SetFontItem(aIndex: Integer; AValue: TFont);
+begin
+
+end;
+
+procedure TOptionsForm.InitFonts;
+begin
+  //SetLength(FFontItemList, High(TUIFontItem));
 end;
 
 function TOptionsForm.ShowModalPage(page: TOptionsPage): TModalResult;
