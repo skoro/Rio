c3d28678a172e20fcd0e22400c5bf61d4eb66358
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-08-20 22:34:34 +0300

    Initial focus on text edit. Pressing Esc causes to close the form.

--- source/help_form.pas
+++ source/help_form.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Forms, ButtonPanel,
-  StdCtrls;
+  StdCtrls, Classes, Controls;
 
 type
 
@@ -15,6 +15,8 @@ type
   THelpForm = class(TForm)
     bpButtons: TButtonPanel;
     helpText: TMemo;
+    procedure FormKeyPress(Sender: TObject; var Key: char);
+    procedure FormShow(Sender: TObject);
   private
 
   public
@@ -26,6 +28,17 @@ implementation
 {$R *.lfm}
 
 { THelpForm }
+
+procedure THelpForm.FormShow(Sender: TObject);
+begin
+  helpText.SetFocus;
+end;
+
+procedure THelpForm.FormKeyPress(Sender: TObject; var Key: char);
+begin
+  if Key = #27 then
+    Close;
+end;
 
 function THelpForm.ShowModal(ACaption, AHelpText: string): TModalResult;
 begin
