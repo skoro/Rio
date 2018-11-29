8d966ddb92eb9b51e5fb2fb6d8edf6318634a42e
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-11-29 22:39:15 +0200

    Check that a tab is currently open and a selected value isn't empty.

--- source/main.pas
+++ source/main.pas
@@ -685,8 +685,11 @@ end;
 procedure TForm1.miFindClick(Sender: TObject);
 var
   tab: TResponseTab;
+  Sel: string;
+  Page: TTabSheet;
 begin
-  if pagesResponse.ActivePage = tabContent then begin
+  Page := pagesResponse.ActivePage;
+  if Page = tabContent then begin
     // Autofill find text from the raw content tab.
     if responseRaw.SelText <> '' then
       dlgFind.FindText := responseRaw.SelText;
@@ -694,8 +697,11 @@ begin
   else begin
     // Autofill find text from the response tab.
     tab := FResponseTabManager.CanFind;
-    if (tab <> Nil) and (tab is IEditorSelectedText) then
-      dlgFind.FindText := (tab as IEditorSelectedText).SelectedText;
+    if (tab <> Nil) and (Page = tab.TabSheet) and (tab is IEditorSelectedText) then begin
+      Sel := (tab as IEditorSelectedText).SelectedText;
+      if Sel <> '' then
+        dlgFind.FindText := Sel;
+    end;
   end;
   dlgFind.Execute;
 end;
