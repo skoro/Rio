104cfc708522e1c841dbeeb1171fd87cfe4bce0c
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2019-02-04 21:11:09 +0200

    Time values axis.

--- source/profiler_graph.pas
+++ source/profiler_graph.pas
@@ -17,6 +17,7 @@ type
     FChart: TChart;
     FListChartSource: TListChartSource;
     FListChartSourceLABELS: TListChartSource;
+    FListChartSourceVALUES: TListChartSource;
     FTimeSeries: TBoxAndWhiskerSeries;
     FTimeCheckPoints: TTimeCheckPointList;
     procedure SetTimeCheckPoints(AValue: TTimeCheckPointList);
@@ -33,7 +34,7 @@ type
 
 implementation
 
-uses dateutils, TAChartUtils;
+uses dateutils, TAChartUtils, TAChartAxisUtils;
 
 
 { TProfilerGraph }
@@ -55,6 +56,7 @@ begin
   FListChartSource := TListChartSource.Create(FChart);
   FListChartSource.YCount := 5;
   FListChartSourceLABELS := TListChartSource.Create(FChart);
+  FListChartSourceVALUES := TListChartSource.Create(FChart);
   FTimeSeries := TBoxAndWhiskerSeries.Create(FChart);
   FTimeSeries.Source := FListChartSource;
   FTimeSeries.AxisIndexX := 0;
@@ -62,6 +64,11 @@ begin
   FChart.AddSeries(FTimeSeries);
   FChart.LeftAxis.Marks.Source := FListChartSourceLABELS;
   FChart.LeftAxis.Marks.Style := smsLabel;
+  with FChart.AxisList.Add do begin
+    Alignment := calRight;
+    Marks.Source := FListChartSourceVALUES;
+    Marks.Style := smsLabel;
+  end;
 end;
 
 procedure TProfilerGraph.SetProfilerData;
@@ -76,6 +83,7 @@ var
 begin
   FListChartSource.Clear;
   FListChartSourceLABELS.Clear;
+  FListChartSourceVALUES.Clear;
   if FTimeCheckPoints.Count = 0 then
     Exit;
 
@@ -87,21 +95,25 @@ begin
   f := GetOffset(cp);
   FListChartSource.AddXYList(0, [0, f, 0, f + cp.Duration, total.Duration]);
   FListChartSourceLABELS.Add(0, 0, 'Connect');
+  FListChartSourceVALUES.Add(0, 0, Format('%d ms', [cp.Duration]));
 
   cp := FTimeCheckPoints.KeyData['Send request'];
   f := GetOffset(cp);
   FListChartSource.AddXYList(1, [0, f, 0, f + cp.Duration, total.Duration]);
   FListChartSourceLABELS.Add(0, 1, 'Send request');
+  FListChartSourceVALUES.Add(0, 1, Format('%d ms', [cp.Duration]));
 
   hdr := FTimeCheckPoints.KeyData['Response headers'];
   f := GetOffset(hdr);
   FListChartSource.AddXYList(2, [0, f, 0, f + hdr.Duration, total.Duration]);
   FListChartSourceLABELS.Add(0, 2, 'Headers');
+  FListChartSourceVALUES.Add(0, 2, Format('%d ms', [hdr.Duration]));
 
   // Response is a response body + the response headers.
   cp := FTimeCheckPoints.KeyData['Read response'];
   FListChartSource.AddXYList(3, [0, f + hdr.Duration, 0, f + hdr.Duration + (cp.Duration - hdr.Duration), total.Duration]);
   FListChartSourceLABELS.Add(0, 3, 'Response');
+  FListChartSourceVALUES.Add(0, 3, Format('%d ms', [cp.Duration - hdr.Duration]));
 end;
 
 constructor TProfilerGraph.Create(AParent: TWinControl);
