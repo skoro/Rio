f8adae2284f051209a449ebcda526d02f34dce98
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2019-06-30 16:39:44 +0300

    Fix #115: Bookmark tree icons.

--- source/main.pas
+++ source/main.pas
@@ -755,6 +755,9 @@ begin
   // Bookmark manager initialization.
   FBookManager := TBookmarkManager.Create(Self);
   FBookManager.Parent := BookmarkSide;
+  FBookManager.TreeView.StateImages := toolbarIcons;
+  FBookManager.ImageIndexFolder := 6;
+  FBookManager.ImageIndexBookmark := 7;
   FBookManager.OnChangeBookmark := @OnChangeBookmark;
   FBookManager.Popup.OnEditClick := @BookmarkEditorShow;
   FBookManager.Popup.OnDeleteClick := @OnDeleteBookmark;
