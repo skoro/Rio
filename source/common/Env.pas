8d3a6f3686fd4f1897a429699a19d8dadd9a59de
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2019-10-27 21:59:55 +0200

    EnvManager

--- source/common/Env.pas
+++ source/common/Env.pas
@@ -45,23 +45,86 @@ type
   { TEnvManager }
 
   TEnvManager = class
+  private
+    type
+      TEnvList = specialize TFPGObjectList<TEnvironment>;
+  private
+    FEnvList: TEnvList;
+    function GetEnv(EnvName: string): TEnvironment;
+    function GetEnvNames: TStringArray;
+  protected
+    function FindEnv(const EnvName: string): TEnvironment;
   public
     constructor Create; virtual;
     destructor Destroy; override;
+    procedure Add(const Env: TEnvironment);
+    procedure Delete(const EnvName: string);
+    property EnvNames: TStringArray read GetEnvNames;
+    property Env[EnvName: string]: TEnvironment read GetEnv; default;
   end;
 
 implementation
 
 { TEnvManager }
 
+function TEnvManager.GetEnv(EnvName: string): TEnvironment;
+begin
+  Result := FindEnv(EnvName);
+  if not Assigned(Result) then
+    raise Exception.CreateFmt('Environment "%s" not found.', [EnvName]);
+end;
+
+function TEnvManager.GetEnvNames: TStringArray;
+var
+  i: integer;
+begin
+  SetLength(Result, FEnvList.Count);
+  for i := 0 to FEnvList.Count - 1 do
+    Result[i] := FEnvList[i].Name;
+end;
+
+function TEnvManager.FindEnv(const EnvName: string): TEnvironment;
+begin
+  for Result in FEnvList do
+    if Result.Name = EnvName then
+      Exit;
+  Result := nil;
+end;
+
 constructor TEnvManager.Create;
 begin
   inherited;
+  FEnvList := TEnvList.Create(True);
 end;
 
 destructor TEnvManager.Destroy;
 begin
+  FreeAndNil(FEnvList);
   inherited Destroy;
+end;
+
+procedure TEnvManager.Add(const Env: TEnvironment);
+begin
+  if FindEnv(Env.Name) <> nil then
+    raise Exception.CreateFmt('Environment "%s" is already exist.', [Env.Name]);
+  FEnvList.Add(Env);
+end;
+
+procedure TEnvManager.Delete(const EnvName: string);
+var
+  i: integer;
+  e: TEnvironment;
+begin
+  E := nil;
+  for i := 0 to FEnvList.Count - 1 do
+    if FEnvList[i].Name = EnvName then
+    begin
+      E := FEnvList[i];
+      break;
+    end;
+  if E = nil then
+    raise Exception.CreateFmt('Environment "%s" not found.', [EnvName]);
+  FEnvList.Delete(i);
 end;
 
 { EVariableNotFound }
